ed7a14db1af013cc0d68ef3fba36be564f11f1f5
Guma <giltonguma@gmail.com>
2022-05-22 20:39:00 -0300

    update

--- js/index.js
+++ js/index.js
@@ -2,10 +2,16 @@ window.onload = function() {
   var items = document.getElementsByClassName('item');
   var items_lenght = items.length;
   var count = 0;
+  var total = 0;
 
   for (var i = 0; i < items_lenght; i++) {
     count = count + parseInt(items[i].getAttribute("data-count"));
+    
+    if (items[i].getAttribute("data-price") != null) {
+      total = total + parseInt(items[i].getAttribute("data-price"));
+    }
   }
 
   document.getElementById('total').innerHTML = count;
+  document.getElementById('total-price').innerHTML = "R$ " + total.toLocaleString('pt-br', {minimumFractionDigits: 2});
 }
