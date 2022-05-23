d5215107e0b1cc8d5e6d30fa6d62be81a472e6cc
Guma <guma@email.com>
2022-05-23 20:16:40 -0300

    Update

--- js/index.js
+++ js/index.js
@@ -1,20 +1,39 @@
-window.onload = function() {
-  var items = document.getElementsByClassName('item');
-  var items_lenght = items.length;
+window.onload = function () {
   var count = 0;
   var total = 0;
+  var items = document.getElementsByClassName('item');
+  var items_lenght = items.length;
 
   for (var i = 0; i < items_lenght; i++) {
-    count = count + parseInt(items[i].getAttribute('data-count'));
-    
-    if (items[i].getAttribute('data-price') != null) {
-      price = parseInt(items[i].getAttribute('data-price'));
-      total = total + price;
+    let item = items[i];
+    count = count + parseInt(item.getAttribute('data-count'));
 
+    let title_element = item.getElementsByClassName('title')[0];
+    let title_text = title_element.innerText;
+    let title_url = title_text.toLowerCase();
+
+    if (title_url.includes("maisto")) {
+      title_url = `1:64 ${title_url}`;
+    } else if (title_url.includes("greenlight")) {
+      title_text = `1:64 ${title_url}`;
+    } else if (title_url.includes("matchbox")) {
+      title_url = `1:64 ${title_url}`;
+    } else {
+      title_url = `hot wheels 1:64 ${title_url}`;
+    }
+
+    title_url = `https://google.com/search?q=${encodeURIComponent(title_url).replace('%20', '+')}`;
+    title_element.innerHTML = `<a target="_blank" href="${title_url}">${title_text}</a>`;
+
+    if (item.getAttribute('data-price') != null) {
+      let price = parseInt(item.getAttribute('data-price'));
+
+      total = total + price;
       price = price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
-      price_element = document.createElement('spam');
+
+      let price_element = document.createElement('spam');
       price_element.innerHTML = `<span class="price">${price}</spam>`;
-      title_element = items[i].getElementsByClassName('title')[0];
+
       title_element.appendChild(price_element);
     }
   }
